848638d55e7818b0b098aa88103a955bd0d3a024
unknown <bratwelsniper_2051@hotmail.com>
2025-10-31 02:45:02 -0500

    chatbot-medico

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-
+import os 
 app = Flask(__name__)
 
 @app.route("/webhook", methods=['POST'])
@@ -34,5 +34,5 @@ def home():
     return "Chatbot médico funcionando!"
 
 if __name__ == "__main__":
-    print("Iniciando chatbot...")
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
